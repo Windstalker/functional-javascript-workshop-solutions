a761e6186e346f49cb988dc35008e4308538e3a7
Alex Subota <as.subota@gmail.com>
2016-07-25 13:46:35 +0300

    Solve task #2

---
+++ 2.js
@@ -0,0 +1,14 @@
+// function repeat(operation, num) {
+//   let count = num;
+//   while (count--) {
+//     operation();
+//   }
+// }
+
+function repeat(operation, num) {
+  if (num <= 0) return;
+  operation();
+  return repeat(operation, num - 1);
+}
+
+module.exports = repeat;
